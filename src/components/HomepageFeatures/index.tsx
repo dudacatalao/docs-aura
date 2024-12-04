790679542b615ddc5f1ea4ef0887cb8ca50be2b5
Maria Eduarda <dmelocatalao@gmail.com>
2024-12-04 00:49:04 -0300

    bug: teste

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -8,8 +8,38 @@ type FeatureItem = {
   description: JSX.Element;
 };
 
-// Não usamos mais uma lista FeatureList
-function Feature({ title, Svg, description }: FeatureItem) {
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Totem',
+    Svg: require('@site/static/img/totem.svg').default,
+    description: (
+      <>
+        Totem de Pré-triagem de Acesso aos recusos do ambuatorio.
+      </>
+    ),
+  },
+  {
+    title: 'Sistema Interno',
+    Svg: require('@site/static/img/sistema.svg').default,
+    description: (
+      <>
+        Sistema Interno para acompanhamento dos pacientes e gerenciamento interno.
+      </>
+    ),
+  },
+  {
+    title: 'Visor',
+    Svg: require('@site/static/img/visor.svg').default,
+    description: (
+      <>
+        
+        Visor para exibição de senhas.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -28,38 +58,9 @@ export default function HomepageFeatures(): JSX.Element {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {/* Renderizando as Features uma por uma */}
-
-          <Feature
-            title="Totem"
-            Svg={require('@site/static/img/totemm.svg').default}
-            description={
-              <>
-                Totem de Pré-triagem de Acesso aos recursos do ambulatório.
-              </>
-            }
-          />
-
-          <Feature
-            title="Sistema Interno"
-            Svg={require('@site/static/img/visorr.svg').default}
-            description={
-              <>
-                Sistema Interno para acompanhamento dos pacientes e gerenciamento interno.
-              </>
-            }
-          />
-
-          <Feature
-            title="Visor"
-            Svg={require('@site/static/img/undraw_docusaurus_react.svg').default}
-            description={
-              <>
-                Visor para exibição de senhas.
-              </>
-            }
-          />
-
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
         </div>
       </div>
     </section>
