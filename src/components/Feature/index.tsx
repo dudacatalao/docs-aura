f33006ab6de6f9072f19a1aa4dd41d9f606a2b2a
dudacatalao <dmelocatalao@gmail.com>
2024-12-04 09:22:33 -0300

    :bento: feat: update functions

---
+++ src/components/Feature/index.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+
+export default function Feature(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
